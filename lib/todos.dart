93569bfefdb6470005fa54a2d2f00cb5f282929b
akshaygupta1407 <85879402+akshaygupta1407@users.noreply.github.com>
2021-12-04 15:00:03 +0530

    Update todos.dart

--- lib/todos.dart
+++ lib/todos.dart
@@ -4,18 +4,6 @@ import 'todo.dart';
 class TodosProvider extends ChangeNotifier{
 
   List<Todo> _todos = [
-    Todo(createdTime: DateTime.now(), title: "Avengers", id: '', description: 'Joe Russo',
-        image: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg',
-    ),
-    Todo(createdTime: DateTime.now(), title: "Avengers1", id: '', description: 'Joe Russo',
-        image: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg',
-       ),
-    Todo(createdTime: DateTime.now(), title: "Avengers2", id: '', description: 'Joe Russo',
-        image: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg',
-    ),
-    Todo(createdTime: DateTime.now(), title: "Avengers3", id: '', description: 'Joe Russo',
-        image: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg',
-        ),
   ];
   List<Todo> get todos => _todos;
   //where((todo)=>todo.isDone==false).toList();
